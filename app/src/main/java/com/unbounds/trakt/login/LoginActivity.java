f18a947216641eeb8ad417bfc0c9095b95ac53ec
Alireza Pazirandeh <maclir@gmail.com>
2017-03-08 12:52:02 +0100

    Use chrome custom tabs
    For some reason the custom tabs activity stays in the stack. This has to
    be fixed later.

--- app/src/main/java/com/unbounds/trakt/login/LoginActivity.java
+++ app/src/main/java/com/unbounds/trakt/login/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.unbounds.trakt.login;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.unbounds.trakt.ApiWrapper;
 import com.unbounds.trakt.BuildConfig;
@@ -27,23 +28,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final WebView webView = (WebView) findViewById(R.id.login_webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient() {
+
+        final CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
             @Override
-            public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
-                if (url.startsWith(getString(R.string.oauth_referrer))) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                    return true;
-                }
-                return false;
+            public void onCustomTabsServiceConnected(final ComponentName componentName, final CustomTabsClient client) {
+                final CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                final CustomTabsIntent customTabsIntent = builder.build();
+                client.warmup(0L); // This prevents backgrounding after redirection
+                customTabsIntent.launchUrl(LoginActivity.this, Uri.parse(String.format("%s/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s",
+                        BuildConfig.BASE_URL,
+                        BuildConfig.CLIENT_ID,
+                        getString(R.string.oauth_referrer))));
             }
-        });
-        webView.loadUrl(String.format("%s/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s",
-                BuildConfig.BASE_URL,
-                BuildConfig.CLIENT_ID,
-                getString(R.string.oauth_referrer)));
+
+            @Override
+            public void onServiceDisconnected(final ComponentName name) {
+            }
+        };
+        CustomTabsClient.bindCustomTabsService(this, "com.android.chrome", connection);
     }
 
     @Override
