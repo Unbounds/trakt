c447ed6ec07387fb253dc61e91f9323a649f76e3
Alireza Pazirandeh <maclir@gmail.com>
2018-09-04 17:18:41 +0200

    Cleanup project

--- app/src/main/java/com/unbounds/trakt/progress/Adapter.java
+++ app/src/main/java/com/unbounds/trakt/progress/Adapter.java
@@ -27,142 +27,145 @@ import rx.functions.Action1;
  * Created by maclir on 2015-11-17.
  */
 class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    private final List<WatchedProgressWrapper> mData = new ArrayList<>();
-    private final Context mContext;
 
-    Adapter(final Context context) {
-        mContext = context;
-    }
+	private final List<WatchedProgressWrapper> mData = new ArrayList<>();
+	private final Context mContext;
 
-    @Override
-    public Adapter.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        final View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.progress_item, parent, false);
-        return new ViewHolder(view, new ViewHolder.OnClicked() {
-            @Override
-            public void onCheckClicked(final int position) {
-                final WatchedProgressWrapper watchedProgressWrapper = mData.get(position);
-                watchedProgressWrapper.setSelected(true);
-                final Episode episode = watchedProgressWrapper.getWatchedProgress().getNextEpisode();
-                ApiWrapper.postWatchedItems(new WatchedItems.Builder().addEpisode(episode).create()).subscribe(new Action1<AddHistory>() {
-                    @Override
-                    public void call(final AddHistory addHistory) {
-                        //TODO check if added episode exists in not_added
-                        ApiWrapper.getWatchedProgress(watchedProgressWrapper.getShow().getIds().getTrakt()).subscribe(new Action1<WatchedProgress>() {
-                            @Override
-                            public void call(final WatchedProgress watchedProgress) {
-                                watchedProgressWrapper.setSelected(false);
-                                if (watchedProgress.isCompleted()) {
-                                    mData.remove(position);
-                                    notifyItemRemoved(position);
-                                } else {
-                                    mData.set(position, watchedProgressWrapper.setWatchedProgress(watchedProgress));
-                                    notifyItemChanged(position);
-                                }
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    }
+	Adapter(final Context context) {
+		mContext = context;
+	}
 
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        final WatchedProgressWrapper watchedProgressWrapper = mData.get(position);
-        final WatchedProgress watchedProgress = watchedProgressWrapper.getWatchedProgress();
-        final Show show = watchedProgressWrapper.getShow();
-        final boolean selected = watchedProgressWrapper.isSelected();
+	@Override
+	public Adapter.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+		final View view = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.progress_item, parent, false);
+		return new ViewHolder(view, new ViewHolder.OnClicked() {
+			@Override
+			public void onCheckClicked(final int position) {
+				final WatchedProgressWrapper watchedProgressWrapper = mData.get(position);
+				watchedProgressWrapper.setSelected(true);
+				final Episode episode = watchedProgressWrapper.getWatchedProgress().getNextEpisode();
+				ApiWrapper.postWatchedItems(new WatchedItems.Builder().addEpisode(episode).create()).subscribe(new Action1<AddHistory>() {
+					@Override
+					public void call(final AddHistory addHistory) {
+						//TODO check if added episode exists in not_added
+						ApiWrapper.getWatchedProgress(watchedProgressWrapper.getShow().getIds().getTrakt()).subscribe(new Action1<WatchedProgress>() {
+							@Override
+							public void call(final WatchedProgress watchedProgress) {
+								watchedProgressWrapper.setSelected(false);
+								if (watchedProgress.isCompleted()) {
+									mData.remove(position);
+									notifyItemRemoved(position);
+								} else {
+									mData.set(position, watchedProgressWrapper.setWatchedProgress(watchedProgress));
+									notifyItemChanged(position);
+								}
+							}
+						});
+					}
+				});
+			}
+		});
+	}
 
-        holder.mLoaded = watchedProgress != null && !watchedProgress.isCompleted();
-        holder.mShowTitle.setText(show.getTitle());
-        Picasso.with(mContext).load(show.getImages().getPoster().getThumb()).into(holder.mShowPoster);
+	@Override
+	public void onBindViewHolder(final ViewHolder holder, final int position) {
+		final WatchedProgressWrapper watchedProgressWrapper = mData.get(position);
+		final WatchedProgress watchedProgress = watchedProgressWrapper.getWatchedProgress();
+		final Show show = watchedProgressWrapper.getShow();
+		final boolean selected = watchedProgressWrapper.isSelected();
 
-        if (holder.mLoaded) {
-            holder.mEpisodeTitle.setText(String.format("S%02dE%02d: %s",
-                    watchedProgress.getNextEpisode().getSeason(),
-                    watchedProgress.getNextEpisode().getNumber(),
-                    watchedProgress.getNextEpisode().getTitle()));
-            final int percentage = (int) ((double) watchedProgress.getCompleted() * 100 / watchedProgress.getAired());
-            holder.mProgressText.setText(String.format("%d/%d (%d%%)", watchedProgress.getCompleted(), watchedProgress.getAired(), percentage));
-            holder.mProgressBar.setProgress(percentage);
-            holder.mCheck.setEnabled(true);
-            holder.mCheck.setSelected(selected);
-        } else {
-            holder.mEpisodeTitle.setText(mContext.getText(R.string.progress_loading));
-            holder.mProgressText.setText("");
-            holder.mProgressBar.setProgress(0);
-            holder.mCheck.setEnabled(false);
-            holder.mCheck.setSelected(selected);
-        }
-    }
+		holder.mLoaded = watchedProgress != null && !watchedProgress.isCompleted();
+		holder.mShowTitle.setText(show.getTitle());
+		Picasso.get().load(show.getImages().getPoster().getThumb()).into(holder.mShowPoster);
 
-    @Override
-    public int getItemCount() {
-        return mData.size();
-    }
+		if (holder.mLoaded) {
+			holder.mEpisodeTitle.setText(String.format("S%02dE%02d: %s",
+					watchedProgress.getNextEpisode().getSeason(),
+					watchedProgress.getNextEpisode().getNumber(),
+					watchedProgress.getNextEpisode().getTitle()));
+			final int percentage = (int) ((double) watchedProgress.getCompleted() * 100 / watchedProgress.getAired());
+			holder.mProgressText.setText(String.format("%d/%d (%d%%)", watchedProgress.getCompleted(), watchedProgress.getAired(), percentage));
+			holder.mProgressBar.setProgress(percentage);
+			holder.mCheck.setEnabled(true);
+			holder.mCheck.setSelected(selected);
+		} else {
+			holder.mEpisodeTitle.setText(mContext.getText(R.string.progress_loading));
+			holder.mProgressText.setText("");
+			holder.mProgressBar.setProgress(0);
+			holder.mCheck.setEnabled(false);
+			holder.mCheck.setSelected(selected);
+		}
+	}
 
-    public void setWatchedProgressWrappers(final List<WatchedProgressWrapper> watchedProgressWrappers) {
-        mData.clear();
-        mData.addAll(watchedProgressWrappers);
-        notifyDataSetChanged();
-    }
+	@Override
+	public int getItemCount() {
+		return mData.size();
+	}
 
-    public void add(final WatchedProgressWrapper watchedProgressWrapper) {
-        final int position = mData.size();
-        mData.add(watchedProgressWrapper);
-        notifyItemInserted(position);
-    }
+	public void setWatchedProgressWrappers(final List<WatchedProgressWrapper> watchedProgressWrappers) {
+		mData.clear();
+		mData.addAll(watchedProgressWrappers);
+		notifyDataSetChanged();
+	}
 
-    public void notifyWatchedProgressWrapperChanged(final WatchedProgressWrapper watchedProgressWrapper) {
-        final int position = mData.indexOf(watchedProgressWrapper);
-        if (position != -1) {
-            notifyItemChanged(position);
-        }
-    }
+	public void add(final WatchedProgressWrapper watchedProgressWrapper) {
+		final int position = mData.size();
+		mData.add(watchedProgressWrapper);
+		notifyItemInserted(position);
+	}
 
-    public void remove(final WatchedProgressWrapper watchedProgressWrapper) {
-        final int position = mData.indexOf(watchedProgressWrapper);
-        if (position != -1) {
-            mData.remove(position);
-            notifyItemRemoved(position);
-        }
-    }
+	public void notifyWatchedProgressWrapperChanged(final WatchedProgressWrapper watchedProgressWrapper) {
+		final int position = mData.indexOf(watchedProgressWrapper);
+		if (position != -1) {
+			notifyItemChanged(position);
+		}
+	}
 
-    public void clear() {
-        mData.clear();
-        notifyDataSetChanged();
-    }
+	public void remove(final WatchedProgressWrapper watchedProgressWrapper) {
+		final int position = mData.indexOf(watchedProgressWrapper);
+		if (position != -1) {
+			mData.remove(position);
+			notifyItemRemoved(position);
+		}
+	}
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        public interface OnClicked {
-            void onCheckClicked(final int position);
-        }
+	public void clear() {
+		mData.clear();
+		notifyDataSetChanged();
+	}
 
-        private final TextView mShowTitle;
-        private final TextView mEpisodeTitle;
-        private final ImageView mShowPoster;
-        private final TextView mProgressText;
-        private final ProgressBar mProgressBar;
-        private final View mCheck;
-        private boolean mLoaded;
+	static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(final View view, final OnClicked listener) {
-            super(view);
-            mShowPoster = (ImageView) view.findViewById(R.id.progress_item_show_poster);
-            mShowTitle = (TextView) view.findViewById(R.id.progress_item_show_title);
-            mEpisodeTitle = (TextView) view.findViewById(R.id.progress_item_episode_title);
-            mProgressBar = (ProgressBar) view.findViewById(R.id.progress_item_progress_bar);
-            mProgressText = (TextView) view.findViewById(R.id.progress_item_progress_text);
-            mCheck = view.findViewById(R.id.progress_item_check);
-            mCheck.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View view) {
-                    if (mCheck.isSelected() || !mLoaded) return;
-                    mCheck.setSelected(true);
-                    listener.onCheckClicked(getAdapterPosition());
-                }
-            });
-        }
-    }
+		public interface OnClicked {
+
+			void onCheckClicked(final int position);
+		}
+
+		private final TextView mShowTitle;
+		private final TextView mEpisodeTitle;
+		private final ImageView mShowPoster;
+		private final TextView mProgressText;
+		private final ProgressBar mProgressBar;
+		private final View mCheck;
+		private boolean mLoaded;
+
+		public ViewHolder(final View view, final OnClicked listener) {
+			super(view);
+			mShowPoster = (ImageView) view.findViewById(R.id.progress_item_show_poster);
+			mShowTitle = (TextView) view.findViewById(R.id.progress_item_show_title);
+			mEpisodeTitle = (TextView) view.findViewById(R.id.progress_item_episode_title);
+			mProgressBar = (ProgressBar) view.findViewById(R.id.progress_item_progress_bar);
+			mProgressText = (TextView) view.findViewById(R.id.progress_item_progress_text);
+			mCheck = view.findViewById(R.id.progress_item_check);
+			mCheck.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(final View view) {
+					if (mCheck.isSelected() || !mLoaded) return;
+					mCheck.setSelected(true);
+					listener.onCheckClicked(getAdapterPosition());
+				}
+			});
+		}
+	}
 }
