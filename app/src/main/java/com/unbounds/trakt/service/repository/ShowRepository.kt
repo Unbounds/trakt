89788c7f14d8d7d62cf700035a8a599ee176bd38
Alireza Pazirandeh <maclir@gmail.com>
2020-08-18 16:17:38 +0200

    Move API calls to repository

--- app/src/main/java/com/unbounds/trakt/service/repository/ShowRepository.kt
+++ app/src/main/java/com/unbounds/trakt/service/repository/ShowRepository.kt
@@ -1,9 +1,10 @@
 package com.unbounds.trakt.service.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.map
+import androidx.lifecycle.*
+import com.unbounds.trakt.service.api.IMAGE_PREFIX
+import com.unbounds.trakt.service.api.TmdbApi
 import com.unbounds.trakt.service.api.TraktApi
+import com.unbounds.trakt.service.api.model.trakt.Show
 import com.unbounds.trakt.service.api.model.trakt.request.Episode
 import com.unbounds.trakt.service.api.model.trakt.request.EpisodeIds
 import com.unbounds.trakt.service.api.model.trakt.request.WatchedItems
@@ -12,34 +13,105 @@ import com.unbounds.trakt.service.api.model.trakt.response.WatchedShow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.collections.set
 
+@Singleton
 class ShowRepository @Inject constructor(
         private val api: TraktApi,
+        private val tmdbApi: TmdbApi,
 ) {
 
-    private val watchedShowsMutable = MutableLiveData<List<WatchedShow>>()
-    val watchedShows: LiveData<List<WatchedShow>> = watchedShowsMutable.map {
-        progressMutables.clear()
-        it
-    }
+    private val refreshingMutable = MutableLiveData<Boolean>()
+    val refreshing: LiveData<Boolean> = refreshingMutable
 
     private val progressMutables = HashMap<Long, MutableLiveData<WatchedProgress>>()
+    private val mediator = MediatorLiveData<List<ShowProgress>>()
+    private val watchedShowsMutable = MutableLiveData<List<WatchedShow>>()
+    val watchedShows = watchedShowsMutable.map { list ->
+        list.map { watchedShow ->
+            watchedShow.show
+        }
+    }.switchMap { list ->
+        progressMutables.clear()
+        mediator.value = listOf()
+        var loadingCounter = 0
 
-    fun getProgress(showId: Long): LiveData<WatchedProgress> = MutableLiveData<WatchedProgress>().apply {
-        progressMutables[showId] = this
+        list.forEach { show ->
+            loadingCounter++
+            val showId = show.ids.trakt
+            val progressMutable = getProgress(showId)
+            progressMutables[showId] = progressMutable
+
+            mediator.addSource(progressMutable) { progress ->
+                if (progress.next_episode != null && !progress.isCompleted) {
+                    with(mediator.value ?: listOf()) {
+                        mediator.value = filterNot { loadedProgress -> loadedProgress.show.ids.trakt == showId }.plus(ShowProgress(show, progress, progress.next_episode))
+                    }
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val tmdbShowId = show.ids.tmdb ?: return@launch
+
+                        val images = tmdbApi.getImages(tmdbShowId.toInt(), progress.next_episode.season.toInt()).await().body()?.posters
+                        if (images.isNullOrEmpty()) return@launch
+
+                        withContext(Dispatchers.Main) {
+                            with(mediator.value ?: listOf()) {
+                                mediator.value = filterNot { loadedProgress -> loadedProgress.show.ids.trakt == showId }.plus(ShowProgress(show, progress, progress.next_episode, "$IMAGE_PREFIX${images.first().file_path}"))
+                            }
+                        }
+                    }
+                } else {
+                    with(mediator.value ?: listOf()) {
+                        mediator.value = filterNot { loadedProgress -> loadedProgress.show.ids.trakt == showId }
+                    }
+                }
+
+                loadingCounter--
+                if (loadingCounter <= 0) {
+                    refreshingMutable.value = false
+                }
+            }
+        }
+
+        mediator
+    }.map { list ->
+        list.sortedByDescending { item -> item.progress.last_watched_at }
+    }.distinctUntilChanged()
+
+    private fun getProgress(showId: Long) = MutableLiveData<WatchedProgress>().apply {
         CoroutineScope(Dispatchers.IO).launch {
             postValue(api.getWatchedProgress(showId).await().body())
         }
     }
 
     fun reload() = CoroutineScope(Dispatchers.IO).launch {
+        refreshingMutable.postValue(true)
         watchedShowsMutable.postValue(listOf())
         watchedShowsMutable.postValue(api.getWatchedShows().await().body() ?: listOf())
     }
 
     fun episodeWatched(showId: Long, episodeId: Long) = CoroutineScope(Dispatchers.IO).launch {
+        withContext(Dispatchers.Main) {
+            with(mediator.value ?: listOf()) {
+                val oldValue = find { loadedProgress -> loadedProgress.show.ids.trakt == showId }
+                        ?: return@with
+                mediator.value = minus(oldValue).plus(
+                        ShowProgress(oldValue.show, oldValue.progress, oldValue.nextEpisode, oldValue.imageUrl, true)
+                )
+            }
+        }
         api.postWatchedItems(WatchedItems(episodes = listOf(Episode(ids = EpisodeIds(trakt = episodeId))))).await()
         progressMutables[showId]?.postValue(api.getWatchedProgress(showId).await().body())
     }
 }
+
+data class ShowProgress(
+        val show: Show,
+        val progress: WatchedProgress,
+        val nextEpisode: com.unbounds.trakt.service.api.model.trakt.Episode,
+        val imageUrl: String? = null,
+        val loading: Boolean = false
+)
--- app/src/main/java/com/unbounds/trakt/viewmodel/ProgressViewModel.kt
+++ app/src/main/java/com/unbounds/trakt/viewmodel/ProgressViewModel.kt
@@ -1,85 +1,40 @@
 package com.unbounds.trakt.viewmodel
 
-import androidx.lifecycle.*
-import com.unbounds.trakt.service.api.IMAGE_PREFIX
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import com.unbounds.trakt.service.repository.ShowRepository
-import com.unbounds.trakt.service.repository.TmdbRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class ProgressViewModel @Inject constructor(private val repository: ShowRepository, private val tmdbRepository: TmdbRepository) : ViewModel() {
+@Singleton
+class ProgressViewModel @Inject constructor(private val repository: ShowRepository) : ViewModel() {
 
+    val refreshing: LiveData<Boolean> = repository.refreshing
 
-    private val mediator = MediatorLiveData<List<NextEpisode>>()
-    val items: LiveData<List<NextEpisode>> = repository.watchedShows.switchMap { list ->
-        mediator.value = listOf()
-
-        var loadingCounter = 0
-
-        list.forEach { watchedShow ->
-            loadingCounter++
-            mediator.addSource(repository.getProgress(watchedShow.show.ids.trakt)) { progress ->
-                val nextEpisode = progress.next_episode
-                if (nextEpisode != null && !progress.isCompleted) {
-                    val loadedList = mediator.value ?: listOf()
-                    val progressPercentage = progress.completed * 100 / progress.aired
-                    val episode = NextEpisode(
-                            lastWatchedAt = progress.last_watched_at,
-                            episodeId = nextEpisode.ids.trakt,
-                            episodeTitle = String.format("S%02dE%02d: %s",
-                                    nextEpisode.season,
-                                    nextEpisode.number,
-                                    nextEpisode.title),
-                            showId = watchedShow.show.ids.trakt,
-                            showTitle = watchedShow.show.title,
-                            progress = progressPercentage.toInt(),
-                            progressText = String.format("%d/%d (%d%%)", progress.completed, progress.aired, progressPercentage),
-                    )
-                    mediator.value = loadedList.filterNot { oldProgress -> oldProgress.showId == episode.showId }.plus(episode)
-
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val showId = watchedShow.show.ids.tmdb ?: return@launch
-                        val images = tmdbRepository.getImages(showId.toInt(), nextEpisode.season.toInt(), nextEpisode.number.toInt())
-                        if (images.isNullOrEmpty()) return@launch
-
-                        withContext(Dispatchers.Main) {
-                            val newList = mediator.value ?: listOf()
-                            mediator.value = newList.filterNot { oldProgress -> oldProgress.showId == episode.showId }.plus(NextEpisode(episode, "$IMAGE_PREFIX${images.first().file_path}"))
-                        }
-                    }
-                }
-                loadingCounter--
-                if (loadingCounter <= 0) {
-                    refreshingMutable.value = false
-                }
-            }
+    val items: LiveData<List<NextEpisode>> = repository.watchedShows.map { list ->
+        list.map { item ->
+            val progressPercentage = item.progress.completed * 100 / item.progress.aired
+            NextEpisode(
+                    lastWatchedAt = item.progress.last_watched_at,
+                    episodeId = item.nextEpisode.ids.trakt,
+                    episodeTitle = String.format("S%02dE%02d: %s",
+                            item.nextEpisode.season,
+                            item.nextEpisode.number,
+                            item.nextEpisode.title),
+                    showId = item.show.ids.trakt,
+                    showTitle = item.show.title,
+                    imageUrl = item.imageUrl,
+                    progress = progressPercentage.toInt(),
+                    progressText = String.format("%d/%d (%d%%)", item.progress.completed, item.progress.aired, progressPercentage),
+                    selected = item.loading,
+            )
         }
-        mediator
-    }.map { list ->
-        list.sortedByDescending { item -> item.lastWatchedAt }
     }
 
-    private val refreshingMutable = MutableLiveData<Boolean>()
-    val refreshing: LiveData<Boolean> = refreshingMutable
+    fun episodeWatched(episode: NextEpisode) = repository.episodeWatched(episode.showId, episode.episodeId)
 
-    fun episodeWatched(episode: NextEpisode) {
-        val loadedList = mediator.value ?: listOf()
-        mediator.value = loadedList.filterNot { oldProgress -> oldProgress.showId == episode.showId }.plus(
-                NextEpisode(
-                        episode,
-                        selected = true,
-                )
-        )
-        repository.episodeWatched(episode.showId, episode.episodeId)
-    }
-
-    fun reload() {
-        repository.reload()
-        refreshingMutable.value = true
-    }
+    fun reload() = repository.reload()
 }
 
 data class NextEpisode(
@@ -90,9 +45,6 @@ data class NextEpisode(
         val showTitle: String,
         val progress: Int,
         val progressText: String,
-        val imageUrl: String? = null,
-        val selected: Boolean = false,
-) {
-    constructor(episode: NextEpisode, selected: Boolean) : this(episode.lastWatchedAt, episode.episodeId, episode.episodeTitle, episode.showId, episode.showTitle, episode.progress, episode.progressText, episode.imageUrl, selected)
-    constructor(episode: NextEpisode, imageUrl: String) : this(episode.lastWatchedAt, episode.episodeId, episode.episodeTitle, episode.showId, episode.showTitle, episode.progress, episode.progressText, imageUrl, episode.selected)
-}
+        val imageUrl: String?,
+        val selected: Boolean,
+)
